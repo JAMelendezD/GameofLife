dfc0afbd798cb2a9f26f6b4ad47e688727d8ad5f
JAMelendezD <ja.melendez10@uniandes.edu.co>
2022-05-16 01:05:40 -0400

    stetics

--- conways.py
+++ conways.py
@@ -1,13 +1,13 @@
-# ---------------------------------------------------------------------------------------
-# conways.py : Conways game of life in python (7 min 50 sec) with numba and numpy (19.76)
-# ---------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# conways.py : Conways game of life in python with numba and numpy (19.76)
+# ------------------------------------------------------------------------------
 
 import numpy as np
 import argparse
 from numba import jit
 import time
 
-@jit(nopython=True)
+@jit(nopython = True)
 def update_world(world):
 
 	old_world = np.copy(world)
@@ -18,37 +18,37 @@ def update_world(world):
 	for i in range(rows):
 		for j in range(cols):
 			state = old_world[i][j]
-			neis = old_world[i%rows][(j+1)%cols]\
-				  +old_world[i%rows][(j-1)%cols]\
-				  +old_world[(i+1)%rows][j%cols]\
-				  +old_world[(i-1)%rows][j%cols]\
-				  +old_world[(i+1)%rows][(j+1)%cols]\
-				  +old_world[(i-1)%rows][(j-1)%cols]\
-				  +old_world[(i+1)%rows][(j-1)%cols]\
-				  +old_world[(i-1)%rows][(j+1)%cols]		
+			neis = old_world[i % rows][(j + 1) % cols]\
+				  +old_world[i % rows][(j - 1) % cols]\
+				  +old_world[(i + 1) % rows][j % cols]\
+				  +old_world[(i - 1)%rows][j % cols]\
+				  +old_world[(i + 1) % rows][(j + 1) % cols]\
+				  +old_world[(i - 1) % rows][(j - 1) % cols]\
+				  +old_world[(i + 1) % rows][(j - 1) % cols]\
+				  +old_world[(i - 1) % rows][(j + 1) % cols]		
 
-			if state == 1:									# Kill if alive and nei !2 or !3
-				if (neis != 2) and neis != 3:
+			if state == 1:									
+				if (neis != 2 and neis != 3):
 					world[i][j] = 0
 			else:
-				if neis == 3:								# Alive if death and nei 3
+				if neis == 3:								
 					world[i][j] = 1
-															# otherwise stay dead or alive
+
 def main():
 
-	world = np.random.randint(0,2,(args.r,args.c))			# Initialize random world
+	world = np.random.randint(0, 2, (args.r, args.c))
 
 	print('Started simulation')
 	start = time.time()
-	for i in range(args.f+1):
-		np.savetxt(f'{i:06d}.txt', world, fmt='%d')			# Save world to a text file
+	for i in range(args.f + 1):
+		np.savetxt(f'{i:06d}.txt', world, fmt='%d')
 		update_world(world)
-	print(f'Finished simulation in: {time.time()-start:6.2f} seconds')
+	print(f'Finished simulation in: {time.time() - start:6.2f} seconds')
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Game of Life')
-	parser.add_argument('r',type=int,help='number of rows')
-	parser.add_argument('c',type=int,help='number of columns')
-	parser.add_argument('f',type=int,help='number of frames')
+	parser = argparse.ArgumentParser(description = 'Game of Life')
+	parser.add_argument('r', type=int, help='number of rows')
+	parser.add_argument('c', type=int, help='number of columns')
+	parser.add_argument('f', type=int, help='number of frames')
 	args = parser.parse_args()
 	main()
--- conways.r
+++ conways.r
@@ -1,67 +1,58 @@
-# ---------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # conways.cs : Conways game of life in R (16 min 11 sec)
-# ---------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
-update_world <- function(world) 
-{
-    
+update_world <- function(world) {
+
     shape <- dim(world)
     rows <- shape[1]
     cols <- shape[2]
-
     old_world <- cbind(world)
 
-    for (i in  1:rows)
-    {
-        for (j in  1:cols)
-        {
+    for (i in  1:rows) {
+        for (j in  1:cols) {
             state <- old_world[i,j]
-			neis <- old_world[(i-1)%%rows+1,j%%cols+1]+
-                    old_world[(i-1)%%rows+1,(j-2)%%cols+1]+
-                    old_world[i%%rows+1,(j-1)%%cols+1]+
-                    old_world[(i-2)%%rows+1,(j-1)%%cols+1]+
-                    old_world[i%%rows+1,j%%cols+1]+
-                    old_world[(i-2)%%rows+1,(j-2)%%cols+1]+
-                    old_world[i%%rows+1,(j-2)%%cols+1]+
-                    old_world[(i-2)%%rows+1,j%%cols+1]
+            neis <- old_world[(i - 1) %% rows + 1, j %% cols + 1] +
+                    old_world[(i - 1) %% rows + 1, (j - 2) %% cols + 1] +
+                    old_world[i %% rows + 1, (j - 1) %% cols + 1] +
+                    old_world[(i - 2) %% rows + 1, (j - 1) %% cols + 1] +
+                    old_world[i %% rows + 1, j %% cols + 1] +
+                    old_world[(i - 2) %% rows + 1, (j - 2) %% cols + 1] +
+                    old_world[i %% rows + 1, (j - 2) %% cols + 1] +
+                    old_world[(i - 2) %% rows + 1, j %% cols + 1]
 
-            if (state == 1)
-            {
-                if ((neis != 2) & (neis != 3))
-                {
+            if (state == 1) {
+                if ((neis != 2) & (neis != 3)) {
                     world[i,j] <- 0
                 }
-            }									            
-			else
-            {
-				if (neis == 3)
-                {
+            }
+            else {
+                if (neis == 3) {
                     world[i,j] <- 1
-                }                           
-            }			
+                }
+            }
         }
     }
     return(world)
 }
 
-main <- function()
-{
+main <- function() {
+
     args <- commandArgs(trailingOnly = TRUE)
     rows <- strtoi(args[1])
     cols <- strtoi(args[2])
     frames <- strtoi(args[3])
 
-    world <- round(matrix(runif(rows*cols), rows, cols))
+    world <- round(matrix(runif(rows * cols), rows, cols))
 
     print("Started simulation")
     start_time <- Sys.time()
-    for (frame in 0:frames)
-    {
+    for (frame in 0:frames) {
         name <- sprintf("%06d.txt", frame)
-        write(world,file=name,ncolumns=cols)
-        world = update_world(world)
+        write(world, file = name, ncolumns = cols)
+        world <- update_world(world)
     }
-    td <- difftime(Sys.time(), start_time, units ="secs")
+    td <- difftime(Sys.time(), start_time, units = "secs")
     sprintf("Finished simulation in: %6.2f seconds", td)
 }
 
