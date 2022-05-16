e83ecbb81e0085479c8b9fd20bb13d874507b052
JAMelendezD <ja.melendez10@uniandes.edu.co>
2022-05-16 01:11:19 -0400

    stetics

--- conways.c
+++ conways.c
@@ -1,25 +1,22 @@
-// ---------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // conways.c : Conways game of life in C (9.35)
-// ---------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
 
-int mod(int a, int b)
-{
-    return (a%b+b)%b;
+int mod(int a, int b) {
+    return (a % b + b) % b;
 }
 
-void write_file(char name[],int rows, int cols, int world[rows][cols])
-{   
+void write_file(char name[], int rows, int cols, int world[rows][cols]) {   
+    
     FILE *fp;
     fp = fopen(name, "w+");
-    for (int i=0; i<rows; ++i)
-    {
-        for (int j=0; j<cols; j++)
-        {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; j++) {
             fprintf(fp, "%d ", world[i][j]);
         }
         fprintf(fp, "\n");
@@ -27,52 +24,43 @@ void write_file(char name[],int rows, int cols, int world[rows][cols])
     fclose(fp);
 }
 
-void init_world(int rows, int cols, int world[rows][cols])
-{    
+void init_world(int rows, int cols, int world[rows][cols]) {    
+    
     srand(time(0));
-
-    for (int i=0; i<rows; ++i)
-    {
-        for (int j=0; j<cols; j++)
-        {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; j++) {
             world[i][j] = rand() % 2;
         }
     }
 }
 
-void update_world(int rows, int cols, int world[rows][cols])
-{    
+void update_world(int rows, int cols, int world[rows][cols]) {    
+    
     int old_world[rows][cols];
     memcpy(old_world, world, sizeof (int) * rows * cols);
     
-    for (int i=0; i<rows; ++i)
-    {
-        for (int j=0; j<cols; j++)
-        {
+    for (int i=0; i<rows; ++i){
+        for (int j=0; j<cols; j++){
             int state = old_world[i][j];                 
-            int neis = old_world[mod(i,rows)][mod(j+1,rows)]
-                      +old_world[mod(i,rows)][mod(j-1,rows)]
-                      +old_world[mod(i+1,rows)][mod(j,rows)]
-                      +old_world[mod(i-1,rows)][mod(j,rows)]
-                      +old_world[mod(i+1,rows)][mod(j+1,rows)]
-                      +old_world[mod(i-1,rows)][mod(j-1,rows)]
-                      +old_world[mod(i+1,rows)][mod(j-1,rows)]
-                      +old_world[mod(i-1,rows)][mod(j+1,rows)];
+            int neis = old_world[mod(i, rows)][mod(j + 1, rows)]
+                      +old_world[mod(i, rows)][mod(j - 1, rows)]
+                      +old_world[mod(i + 1, rows)][mod(j, rows)]
+                      +old_world[mod(i - 1, rows)][mod(j, rows)]
+                      +old_world[mod(i + 1, rows)][mod(j + 1, rows)]
+                      +old_world[mod(i - 1, rows)][mod(j - 1, rows)]
+                      +old_world[mod(i + 1, rows)][mod(j - 1, rows)]
+                      +old_world[mod(i - 1, rows)][mod(j + 1, rows)];
             
-            if (state == 1)                             // Kill if alive and nei !2 or !3
-            {
-                if (neis != 2 && neis != 3)
-                {
+            if (state == 1) {
+                if (neis != 2 && neis != 3) {
                     world[i][j] = 0;
                 }
             }
-            else                                        // Alive if death and nei 3
-            {
-                if (neis == 3)
-                {
+            else {
+                if (neis == 3) {
                     world[i][j] = 1;
                 }  
-            }                                           // Otherwise stay dead or alive 
+            }
         }
     }
 }
@@ -82,18 +70,18 @@ int main (int argc, char **argv)
     int rows = atoi(argv[1]);
     int cols = atoi(argv[2]);
     int frames = atoi(argv[3]);
-    char *name = (char*)malloc(100*sizeof(char));
+    char *name = (char*)malloc(100 * sizeof(char));
 
     int world[rows][cols]; // It is being allocated in the stack limits size significantly
     init_world(rows,cols,world);
     printf("Started simulation\n");
     clock_t start = clock();
-    for (int frame=0; frame<frames+1; frame++)
+    for (int frame = 0; frame < frames + 1; frame++)
     {
         sprintf(name, "%06d.txt", frame);
-        write_file(name,rows,cols,world);
-        update_world(rows,cols,world);  
+        write_file(name, rows, cols, world);
+        update_world(rows, cols, world);  
     }
-    printf("Finished simulation in: %6.2f seconds\n",((double)clock()-start)/CLOCKS_PER_SEC);
+    printf("Finished simulation in: %6.2f seconds\n",((double)clock() - start)/CLOCKS_PER_SEC);
     free(name);
 } 
